21d558ff747334d7b1f0894a3d7b70d5868e88c9
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-07-16 00:29:20 +0000

    added challenge

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -40,6 +40,11 @@ For example: 'Cat' would come before 'apple'
 
 const sortNames = (arr) => {
   // Solution code here...
+  let arr2 = [];
+  arr.forEach ((s) => {
+    arr2.push(s + "!");
+  });
+  return arr2;
 };
 
 /* ------------------------------------------------------------------------------------------------
