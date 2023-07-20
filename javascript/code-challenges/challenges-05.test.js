6e808b248ccc0495459fa996a6b935d77f15d104
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-07-20 01:37:01 +0000

    added challenge 2

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -29,6 +29,7 @@ Write a function named addValues that, given an array of numbers as input, uses 
 
 const addValues = (arr) => {
   // Solution code here...
+  return arr.reduce((acc, currentValue) => acc + currentValue, 0);
 
 };
 
