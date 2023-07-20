35642276012a2294341886b283f8219bc2aa9ae3
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-07-20 02:03:53 +0000

    added challenge 3

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -47,6 +47,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 const addPurchases = (arr) => {
   // Solution code here...
+  return arr.reduce((acc, currentValue) => acc + currentValue.purchasePrice, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
