058bdb53c3c5a36b7d695f2b832e4148b059936f
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-07-20 02:51:20 +0000

    added challenge 6

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -137,9 +137,10 @@ Write a function named reversedString that takes in a string and returns a strin
 
 Note: You must use reduce for this challenge. You may not use the built-in .reverse() string method.
 ------------------------------------------------------------------------------------------------ */
-
 const reversedString = (str) => {
-  // Solution code here...
+   Solution code here...
+   
+   return str.split(").reduce((reversed, char) => char + reversed, ");
 };
 
 /* ------------------------------------------------------------------------------------------------
